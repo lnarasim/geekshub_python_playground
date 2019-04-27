47bd25adf1a9118c32193645b95f9d621646da14
Lakshmi Narayanan Narasimhan <grabyourfreedom@gmail.com>
2019-04-27 22:39:47 +0530

    first commit

---
+++ prime.py
@@ -0,0 +1,54 @@
+from math import inf
+
+# Given a number, the function returns whether or not it is true
+
+def is_prime(number: int):
+    """
+    Function to check whether the number is prime
+
+    Parameters:
+    number (int): An integer that is greater than zero
+
+    Returns:
+    True, if prime
+    False, otherwise
+    """
+    if not number or not isinstance(number, int) or number <= 1:
+        return False
+    if number <= 3:
+        return True
+
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    return True
+
+
+# Given a number, returns the prime number that comes immediately after the number
+
+def next_prime(number: int):
+    """
+    Function that returns the next prime number
+
+    Parameters:
+        number: a positive integer
+
+    Returns:
+        the prime number that follows number
+
+    """
+
+    if not isinstance(number, int):
+        raise ValueError(f"Invalid input {number}")
+
+    if number <= 1:
+        return 2
+
+    start_num = number + 1
+
+    while True:
+        if is_prime(start_num):
+            return start_num
+        start_num += 1
+
+print(next_prime(1000))
---
+++ test_prime.py
@@ -0,0 +1,58 @@
+import pytest
+from prime import is_prime, next_prime
+
+
+def test_is_prime_negative_numbers():
+    assert not is_prime(-1)
+    assert not is_prime(-100)
+    assert not is_prime(-1000)
+
+
+def test_is_prime_non_prime_positive_numbers():
+    assert not is_prime(0)
+    assert not is_prime(1)
+    assert not is_prime(4)
+    assert not is_prime(6)
+    assert not is_prime(8)
+    assert not is_prime(10*12)
+    assert not is_prime(11*13)
+    assert not is_prime(111*119)
+
+
+def test_is_prime_prime_numbers():
+    assert is_prime(2)
+    assert is_prime(5)
+    assert is_prime(7)
+    assert is_prime(11)
+    assert is_prime(131)
+
+
+def test_is_prime_invalid_inputs():
+    assert not is_prime('100')
+    assert not is_prime(1.23)
+    assert not is_prime('10000')
+    assert not is_prime('test')
+
+
+def test_get_next_prime():
+    assert next_prime(1) == 2
+    assert next_prime(2) == 3
+    assert next_prime(-1) == 2
+    assert next_prime(0) == 2
+    assert next_prime(-100) == 2
+    assert next_prime(100) == 101
+    assert next_prime(1000) == 1009
+
+
+def test_get_invalid_input():
+    with pytest.raises(ValueError):
+        next_prime('123')
+
+    with pytest.raises(ValueError):
+        next_prime('121233')
+
+    with pytest.raises(ValueError):
+        next_prime(1.21)
+
+    with pytest.raises(ValueError):
+        next_prime("-inf")
