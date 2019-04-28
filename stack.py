7c63331b981ad9b6192ca5e81d8a927b3d761229
Lakshmi Narayanan Narasimhan <grabyourfreedom@gmail.com>
2019-04-28 22:41:44 +0530

    first commit

---
+++ stack.py
@@ -0,0 +1,40 @@
+# A simple stack implementation with push and pop of objects with stack containing fixed number of slots
+
+DEFAULT_STACK_SIZE = 10
+
+
+class Stack:
+
+    def __init__(self, max_slots=DEFAULT_STACK_SIZE):
+        if not isinstance(max_slots, int) or max_slots < 1:
+            self.max_slots = 10
+        else:
+            self.max_slots = max_slots
+        self._container = []
+
+    def push(self, an_object):
+        if len(self._container) < self.max_slots:
+            self._container.append(an_object)
+        else:
+            raise StackOverflow("Stack is full")
+
+    def pop(self):
+        if len(self._container) > 0:
+            return self._container.pop(len(self._container) - 1)
+        else:
+            raise StackUnderflow("Stack is empty")
+
+    def __len__(self):
+        return len(self._container)
+
+
+class StackOverflow(Exception):
+
+    def __init__(self, message):
+        self.message = message
+
+
+class StackUnderflow(Exception):
+
+    def __init__(self, message):
+        self.message = message
