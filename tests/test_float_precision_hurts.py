5efa9913e5a0669050e07655aed423f7d06a2104
Lakshmi Narayanan Narasimhan <grabyourfreedom@gmail.com>
2019-05-13 19:49:02 +0530

    Example to see where floats gives pain

---
+++ tests/test_float_precision_hurts.py
@@ -0,0 +1,7 @@
+from geekshub_pyproblems.float_precision_hurts import float_precision_issue
+
+
+def test_float_precision_hurts():
+    value = 100.01
+    number = 1000_000_000
+    assert 100010000000 == float_precision_issue(value, number)
